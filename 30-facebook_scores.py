5d866931fa746c5e6897be190133f90002245a36
nardtree <gim.kobayashi@gmail.com>
2018-09-30 19:08:35 +0900

    update

--- 30-facebook_scores.py
+++ 30-facebook_scores.py
@@ -8,14 +8,15 @@ import time
 from datetime import datetime 
 accs = [ line.strip() for line in open('tokens') ]
 
-proxys = [ {'http':f'http://{es[0]}:{es[1]}'} for es in [ line.strip().split() for line in open('proxys') ] ]
-print(proxys)
+#proxys = [ {'http':f'http://{es[0]}:{es[1]}'} for es in [ line.strip().split() for line in open('proxys') ] ]
+#print(proxys)
 if '--scan' in sys.argv:
   import random
   import datetime
   def graph_access(url, acc, obj=None):
     query = f'https://graph.facebook.com/?id={url}&fields=og_object{{engagement}},engagement&access_token={acc}'
-    r     = requests.get(query, proxies=proxy)
+    #r     = requests.get(query, proxies=proxy)
+    r     = requests.get(query)
     fb_obj   = json.loads(r.text)
     tdatetime = datetime.datetime.now()
     eval_time = tdatetime.strftime('%Y-%m-%d %H:%M:%S')
@@ -38,11 +39,13 @@ if '--scan' in sys.argv:
     
   # make loop
   while True:
-    for index, path in enumerate(Path('darturl_clean').glob('*')):
+    paths = list(Path('darturl_clean').glob('*'))
+    size = len(paths)
+    for index, path in enumerate(random.sample(paths, size)):
       key = index%len(accs)
       acc = accs[random.randint(0, len(accs)-1)]
 
-      proxy = proxys[ index%len(proxys) ]
+      #proxy = proxys[ index%len(proxys) ]
       print(acc)
       url   = path.open().read()
       url_hash = sha256(bytes(url,'utf8')).hexdigest() 
